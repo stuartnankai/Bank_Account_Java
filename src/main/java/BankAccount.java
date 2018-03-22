74b6c02621516140ed4e631cfdad898d5a63df19
Jiansun <jians@kth.se>
2018-03-22 16:24:20 +0100

    Clean the code

--- src/main/java/BankAccount.java
+++ src/main/java/BankAccount.java
@@ -2,52 +2,52 @@ public class BankAccount {
 
 
     private int balance;
-    private boolean isopen;
+    private boolean isOpen;
     // open() open account
 
     public void open() {
-        isopen = true;
+        isOpen = true;
     }
 
     // close() close account
 
     public void close() {
-        isopen = false;
+        isOpen = false;
     }
 
-    private boolean ispositive(int amount) {
+    private boolean isPositive(int amount) {
         return amount > 0;
     }
 
-    // deposit()  Cannot deposit or withdraw negative amount
-
     public BankAccount() {
         balance = 0;
-        isopen = false;
+        isOpen = false;
     }
+    // deposit()  Cannot deposit or withdraw negative amount
 
     public synchronized void deposit(int amount) throws BankAccountActionInvalidException {
 
-        if (this.isopen){ // opened account
-            if(ispositive(amount)){ //positive input
-                balance = balance+amount;
-            }else { // negative input
+        if (this.isOpen) { // opened account
+            if (isPositive(amount)) { //positive input
+                balance = balance + amount;
+            } else { // negative input
                 throw new BankAccountActionInvalidException("Cannot deposit or withdraw negative amount");
             }
-        }else { // not open
+        } else { // not open
             throw new BankAccountActionInvalidException("Account closed");
         }
     }
 
-    // withdraw()  Cannot deposit or withdraw negative amount AND Cannot withdraw more money than is currently in the account
+    // withdraw()
+    // Cannot deposit or withdraw negative amount AND Cannot withdraw more money than is currently in the account
     // AND Cannot withdraw money from an empty account
 
     public synchronized void withdraw(int amount) throws BankAccountActionInvalidException {
 
         if (balance == 0) {
             throw new BankAccountActionInvalidException("Cannot withdraw money from an empty account");
-        } else if (ispositive(amount)) {
-            if (this.isopen) {
+        } else if (isPositive(amount)) {
+            if (this.isOpen) {
                 if (balance >= amount) {
                     balance = balance - amount;
                 } else {
@@ -56,17 +56,17 @@ public class BankAccount {
             } else {
                 throw new BankAccountActionInvalidException("Account closed");
             }
-        }else {
+        } else {
             throw new BankAccountActionInvalidException("Cannot deposit or withdraw negative amount");
         }
     }
-    //getBalance()
 
+    //getBalance()
     public synchronized int getBalance() throws BankAccountActionInvalidException {
 
-        if (this.isopen){
+        if (this.isOpen) {
             return balance;
-        }else {
+        } else {
             throw new BankAccountActionInvalidException("Account closed");
         }
     }
